468f0fc582dbf1ce48c164036a44ef7a71798645
webinartelran <webinartelran@gmail.com>
2023-07-31 22:30:12 +0300

    CW20

---
+++ src/main/java/screens/SplashScreen.java
@@ -0,0 +1,12 @@
+package screens;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+
+public class SplashScreen extends BaseScreen{
+
+
+    public SplashScreen(AppiumDriver<MobileElement> driver) {
+        super(driver);
+    }
+}
